536436de247db0200dedf03574522ac3d6decc7e
believeme <cain1227@naver.com>
2025-07-31 22:29:05 +0900

    feat: 2-14 Query Method

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -4,7 +4,10 @@ import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
